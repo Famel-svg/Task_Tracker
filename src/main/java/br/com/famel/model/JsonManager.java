209decf7cdfbcd0fce413e6c8e68a203e9856738
rafaelandradetanimoto <rafaelandradetanimoto@gmail.com>
2025-12-04 16:16:17 +0000

    Inicio do JsonManager

--- src/main/java/br/com/famel/model/JsonManager.java
+++ src/main/java/br/com/famel/model/JsonManager.java
@@ -5,8 +5,23 @@ import java.io.IOException;
 import java.util.List;
 
 public class JsonManager {
-//    Leitura
+//Leitura
 
-//    Edição
+
+//Identa as informações
+private static String indent(int nivel) {
+    return "    ".repeat(nivel);
+}
+
+//Gera o JSON
+public static void salvarEmJson(List<Task> lista) {
+    StringBuilder builder = new StringBuilder();
+
+    // 1. Escrever [
+    // 2. Loop para tasks
+    // 3. Escrever cada objeto
+    // 4. Fechar ]
+    // 5. Gravar no arquivo com FileWriter
+}
 
 }
