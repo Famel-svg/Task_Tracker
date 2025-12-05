cbf896fdc70c42dd46f6c6661c67e448c45468b4
rafaelandradetanimoto <rafaelandradetanimoto@gmail.com>
2025-12-05 15:59:46 +0000

    JsonManager iniciando a função de criação

--- src/main/java/br/com/famel/model/JsonManager.java
+++ src/main/java/br/com/famel/model/JsonManager.java
@@ -16,12 +16,70 @@ private static String indent(int nivel) {
 //Gera o JSON
 public static void salvarEmJson(List<Task> lista) {
     StringBuilder builder = new StringBuilder();
-
+    
     // 1. Escrever [
+    builder.append("[");
+    
     // 2. Loop para tasks
-    // 3. Escrever cada objeto
+    for (int i = 0; i < lista.size(); i++) {
+        Task task = lista.get(i);
+        builder.append("\n");
+        builder.append(indent(1));
+        builder.append("{");
+        builder.append("\n");
+        builder.append(indent(2));
+        builder.append("\"Id\": ");
+        builder.append(task.getId());
+        builder.append(",");
+        builder.append("\n");
+        builder.append(indent(2));
+        builder.append("\"Nome\": ");
+        builder.append("\"");
+        builder.append(task.getNome());
+        builder.append("\"");
+        builder.append(",");
+        builder.append("\n");
+        builder.append(indent(2));
+        builder.append("\"Descricao\": ");
+        builder.append("\"");
+        builder.append(task.getDescricao());
+        builder.append("\"");
+        builder.append(",");
+        builder.append("\n");
+        builder.append(indent(2));
+        builder.append("\"Status\": ");
+        builder.append("\"");
+        builder.append(task.getStatus());
+        builder.append("\"");
+        builder.append(",");
+        builder.append("\n");
+        builder.append(indent(2));
+        builder.append("\"DataDeCriacao\": \"");
+        builder.append(task.getDataDeCriacao());
+        builder.append("\",");
+        builder.append("\n");
+        builder.append(indent(2));
+        builder.append("\"DataDeFinalizacao\": ");
+        builder.append("\"");
+        builder.append(task.getDataDeFinalizacao());
+        builder.append("\"");
+        builder.append("\n");
+        builder.append(indent(1));
+        builder.append("}");
+        // 3. Escrever cada objeto
+        if (i < lista.size() - 1) {
+            builder.append(",");
+        }
+    }
+    
     // 4. Fechar ]
+    builder.append("\n");
+    builder.append("]");
+    
     // 5. Gravar no arquivo com FileWriter
+    try (FileWriter writer = new FileWriter("tasks.json")) {
+        writer.write(builder.toString());
+    }
 }
 
 }
